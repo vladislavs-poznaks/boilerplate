2f78b3ac62bcf784608549e1a242e22265edb406
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-04-16 17:19:47 +0300

    Added validation exception

---
+++ src/Http/Exceptions/ValidationException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Exceptions;
+
+use App\Http\Request;
+use Exception;
+
+class ValidationException extends Exception
+{
+    public function __construct(public Request $request)
+    {
+        parent::__construct();
+    }
+}
