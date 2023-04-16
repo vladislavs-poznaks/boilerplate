9c661561f7424ad34c6f061a338b6c9ecb9753b5
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-04-16 16:29:22 +0300

    Added pre-requisities to run migrations, added database repository

---
+++ src/Repositories/DatabaseRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+
+class DatabaseRepository
+{
+    protected static ?Connection $connection = null;
+
+    protected array $connectionParameters = [];
+
+    public function __construct()
+    {
+        $this->connectionParameters = [
+            'dbname' => $_ENV['DB_DATABASE'],
+            'user' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'host' => $_ENV['DB_HOST'],
+            'driver' => 'pdo_mysql',
+        ];
+    }
+
+    public function getConnection()
+    {
+        return DriverManager::getConnection($this->connectionParameters);
+    }
+}
