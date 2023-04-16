afaed1b721056789daa7953fd347b69a07107390
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-04-16 17:21:26 +0300

    Added register route, switched to response object, added validation exception handling

--- index.php
+++ index.php
@@ -2,23 +2,26 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Exceptions\ValidationException;
+use App\Http\HttpCode;
 use App\Http\Request;
+use App\Http\Response;
 
 $container = require __DIR__ . '/bootstrap.php';
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    $r->addRoute(Request::METHOD_GET, '/api/test', [TestController::class, 'test']);
+    $r->addRoute(Request::METHOD_POST, '/api/register', [RegisterController::class, 'register']);
 });
 
 $route = $dispatcher->dispatch(Request::method(), Request::uri());
 
 switch ($route[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-        http_response_code(404);
+        echo Response::json(['message' => 'Not found'], HttpCode::BAD_REQUEST);
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        http_response_code(405);
+        echo Response::json(['message' => 'Method not allowed'], HttpCode::METHOD_NOT_ALLOWED);
         break;
     case FastRoute\Dispatcher::FOUND:
         [$controller, $method] = $route[1];
@@ -27,11 +30,14 @@ switch ($route[0]) {
 
         try {
             echo $container->call($route[1], $route[2] ?? []);
+        } catch (ValidationException $exception) {
+            echo Response::json($exception->request->errors(), $exception->request->getHttpErrorCode());
         } catch (Exception $exception) {
-            echo json_encode([
-                'message' => $exception->getMessage(),
-                'trace' => $exception->getTrace(),
-            ]);
+            echo Response::json([
+                'message' => 'Server error',
+                'error' => $exception->getMessage(),
+                'trace' => $exception->getTrace()
+            ], HttpCode::INTERNAL_ERROR);
         }
 
         break;
