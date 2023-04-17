185d2bc02d2f2ab8280e8dc645a21433cd35fce4
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:11:00 +0300

    Added carbon orm mapping type

---
+++ src/Types/CarbonType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Types;
+
+use Carbon\Carbon;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class CarbonType extends Type
+{
+    public const NAME = 'carbon';
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        // TODO: Implement getSQLDeclaration() method.
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return Carbon::parse($value);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->toDatetimeString();
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
