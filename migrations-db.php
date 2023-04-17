2f378d9c613ad237b9b97046d04b43e4d302fb6b
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:09:29 +0300

    Switched to static method call in migrations-db

--- migrations-db.php
+++ migrations-db.php
@@ -5,6 +5,4 @@ use App\Repositories\DatabaseRepository;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$repository = new DatabaseRepository();
-
-return $repository->getConnection();
+return DatabaseRepository::connection();
