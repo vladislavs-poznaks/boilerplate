7e58ad621857137da5919cc3b43d896fc4cf9ddd
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:17:14 +0300

    Added user resource

---
+++ src/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\HttpCode;
+use App\Http\Response;
+use App\Models\User;
+
+class UserResource extends Response
+{
+    public function __construct(private User $user)
+    {
+    }
+
+    public static function make(User $user, HttpCode $httpCode = HttpCode::OK): string
+    {
+        $resource = new self($user);
+
+        return self::json([
+            'firstname' => $resource->user->getFirstname(),
+            'lastname' => $resource->user->getLastname(),
+            'email' => $resource->user->getEmail(),
+        ], $httpCode);
+    }
+}
