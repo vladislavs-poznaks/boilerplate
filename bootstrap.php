14f825230dffe1543105ba8952fe315ada7d401a
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:10:08 +0300

    Added carbon orm mapping to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
 use DI\ContainerBuilder;
+use Doctrine\DBAL\Types\Type;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+// Doctrine ORM mapping types
+Type::addType(\App\Types\CarbonType::NAME, \App\Types\CarbonType::class);
 
 // IoC container
 $containerBuilder = new ContainerBuilder;
