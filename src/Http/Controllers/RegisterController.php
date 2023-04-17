711d66d26fc83a5af18ef6352d380920a3706cdc
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:18:48 +0300

    Switched to registration service in register controller

--- src/Http/Controllers/RegisterController.php
+++ src/Http/Controllers/RegisterController.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
-use App\Http\Response;
+use App\Http\Resources\UserResource;
+use App\Services\UserRegistrationService;
 
 class RegisterController
 {
-    public function register(RegisterRequest $request): string
+    public function register(RegisterRequest $request, UserRegistrationService $service): string
     {
-        return Response::json($request->all());
+        return UserResource::make($service->process($request->dto()));
     }
 }
