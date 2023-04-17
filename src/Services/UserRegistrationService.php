b4955bcec29489d1703ef88b2a302ce17b96d7c5
Vladislavs Poznaks <vladislavs.poznaks@viasmsgroup.com>
2023-04-17 23:18:08 +0300

    Added user registration service

---
+++ src/Services/UserRegistrationService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Dto\Users\UserRequestDto;
+use App\Models\User;
+use App\Repositories\Users\UserRepository;
+
+class UserRegistrationService
+{
+    public function __construct(private UserRepository $repository)
+    {
+    }
+
+    public function process(UserRequestDto $dto): User
+    {
+        $user = User::make(
+            $dto->getFirstName(),
+            $dto->getLastName(),
+            $dto->getEmail(),
+            $this->getHashedPassword($dto->getPassword())
+        );
+
+        $this->repository->persistAndSync($user);
+
+        return $user;
+    }
+
+    private function getHashedPassword(string $password): string
+    {
+        return md5($password);
+    }
+}
